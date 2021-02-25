b64d674080ae73ca1d67e747e600c1be80042c03
Daniel <danielhernandez002@hotmail.com>
2021-02-24 22:44:08 -0500

    First charge

---
+++ main.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = "$"
+
+client.once("ready", () => {
+    console.log("Troll bot ready");
+})
+
+client.on("messages", (message) => {
+    console.log(message);
+    if(message.content.starwith(prefix) || message.content.bot){
+        const args = message.content.slice(prefix.length).split(/ +/);
+        const command = args.shift().toUpperCase();
+        if(command === "PING"){
+            message.channel.send('PONG!');
+        } 
+    }
+    else{
+        return null;
+    }
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN);
